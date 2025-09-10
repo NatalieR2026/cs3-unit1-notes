ed15b57ebde7b32aa4ed96a1f6a70b086c91fa1c
NatalieR2026 <natalie.radin2026@gbwl.org>
2025-09-10 13:41:11 +0000

    edited main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("hello world")
+print("hello world")
+
+# Variables store data
+lucky_num = 13 # int
+my_name = "Natalie" # str
+cash = 5.50 # float
+is_raining = True # bool
+is_sunny = False
 
 
-if __name__ == "__main__":
-    main()
