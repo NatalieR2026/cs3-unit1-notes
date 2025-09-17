e33f1932f3a77ca6c0c8066b20c2222989a3ee75
NatalieR2026 <natalie.radin2026@gbwl.org>
2025-09-17 13:41:41 +0000

    9/17 updates to main

--- main.py
+++ main.py
@@ -42,3 +42,82 @@ five_times_three = multiply(5,3)
 print(five_times_three)
 # you can also call the function directly in another
 print(multiply(20,6)) # funcs evaluated from INSIDE to OUTSIDE
+
+# *** LISTS ***
+# ordered, mutable, sequences 
+empty_list = list() # constructor
+another_empty_list = []
+class_roster = ["Bryce", "Finny", "Jackson", "Kevin", "Maia", "Natalie", "Paige"]
+print(class_roster)
+print(len(class_roster))
+# Indexes start at 0 
+first_item = class_roster[0]
+print(first_item)
+# Update an item in a list, access by index
+class_roster[2] = "Jack"
+print(class_roster)
+
+# Sorting lists 
+lottery_nums = [5,148, 12, 589, 1, 999, 32039458, 51]
+print(sorted(lottery_nums))
+print(sorted(lottery_nums, reverse=True))
+print(lottery_nums) # sorted() does not modify OG list
+# Sort IN PLACE -> modifies OG list
+lottery_nums.sort()
+print(lottery_nums)
+class_roster.sort(reverse=True)
+print(class_roster)
+
+# List operations
+class_roster.append("Alex")
+class_roster.insert(0, "Zoie")
+print(class_roster)
+class_roster.remove("Zoie")
+class_roster.pop() # remove last item
+print(class_roster)
+
+# Check if item exists in a list
+print(13 in lottery_nums)
+print(1 in lottery_nums)
+
+# *** TUPLES ***
+# ordered & immutable (can't change items)
+# useful for "snapshot" of a row of data
+student = ('Natalie', 17, 'Computer Science', 4.0)
+print(student)
+# student[3] = 2.6 # CAN'T RE-ASSIGN ITEM!
+
+# *** SETS ***
+# unsorted, stores other immutable types
+# NO DUPLICATED allowed!
+songs = {'Stranger', '3005', '7', '3', 'Mutt', 'Freeze', '3005'}
+print(songs)
+# sets can be used to de-duplicate list items
+colors = ['blue', 'pink', 'purple', 'blue', 'pink']
+print(set(colors))
+# you CAN add/remove items
+songs.add('Gypsy')
+songs.add('Stranger') # duplicate value
+print(songs)
+# look up set OPERATIONS to compare items btwn sets
+
+# *** DICTIONARIES ***
+# mutable, but the KEYS can only be immutable types
+# { key: value } pairs. Keys must be UNIQUE!
+# unordered (no index, can't sort in place)
+characters = { 'Aelin': 'assassin queen',
+              'Karate Kid': 'pupil',
+              'Mr. Miyagi': 'sensei',
+              'Phil Dunphy': 'dad',
+              'Wall-E': 'trash robot',
+              'Princess Peach': 'damsel in distress',
+              'Dexter': 'serial killer (justified)',
+              'Lara Jean': 'letter writer'
+              }
+print(len(characters))
+# dictionary with numerical keys, list values
+grade_requirements = { 9: ['Bio','Math','English','PE'],
+                      10: ['Chem','Math','English','PE'],
+                      11: ['Physics','Math','English','PE'],
+                      12: ['Math','English','PE']
+                      }
